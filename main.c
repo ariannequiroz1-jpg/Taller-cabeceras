eddfcaf5b961119f41d8dba3a9052be04eaa4be4
Laboratorio <laboratorio@udla-ec.int>
2026-04-28 11:36:34 -0500

    Primera fase: creacion de suma

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "sumar"
+int main(){
+    int a,b;
+    printf("Ingrese el 1er numero; ");
+    scanf("%d", &a);
+    printf("Ingrese el 2do numero; ");
+    scanf("%d", &b);
+    return 0;
+
+}
